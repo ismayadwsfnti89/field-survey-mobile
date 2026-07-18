9fe4e813a89a3f9b423a8389fc6377575940a84f
Ismaya <ismayadwsfnti89@gmail.com>
2026-07-17 20:17:57 -0700

    .

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'routes/app_pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: AppPages.router,
+    );
+  }
+}
